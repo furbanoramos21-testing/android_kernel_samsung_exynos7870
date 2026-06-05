2335ab949aac62fabdc79b765c9ead6134c5f647
Andeveloperlab <yogurtbirsah65@gmail.com>
2026-06-05 20:32:34 +0300

    fixup!: cleanup cherry-pick typos

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1592,13 +1592,6 @@ static inline u64 irq_time_read(int cpu)
 }
 #endif /* CONFIG_64BIT */
 #endif /* CONFIG_IRQ_TIME_ACCOUNTING */
-<<<<<<< HEAD
-=======
-
-/*
- * task_may_not_preempt - check whether a task may not be preemptible soon
- */
-extern bool task_may_not_preempt(struct task_struct *task, int cpu);
 
 #ifdef arch_scale_freq_capacity
 #ifndef arch_scale_freq_invariant
@@ -1607,4 +1600,3 @@ extern bool task_may_not_preempt(struct task_struct *task, int cpu);
 #else /* arch_scale_freq_capacity */
 #define arch_scale_freq_invariant()	(false)
 #endif
->>>>>>> ad6408ec74 (BACKPORT: cpufreq: schedutil: New governor based on scheduler utilization data)
